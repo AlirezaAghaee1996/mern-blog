1adc07cf9d9e515a1d4b16c83f499411dfc98fbc
AlirezaAghaee1996 <alireza.aghaee70@gmail.com>
2024-12-14 13:18:08 +0330

    complete

--- backend/Models/CommentMd.js
+++ backend/Models/CommentMd.js
@@ -14,6 +14,10 @@ const commentSchema = new mongoose.Schema(
         type:mongoose.Schema.Types.ObjectId,
         ref:'Post'
 
+    },
+    isActive:{
+        type:Boolean,
+        default:false
     }
   },
   { timestamps: true }
